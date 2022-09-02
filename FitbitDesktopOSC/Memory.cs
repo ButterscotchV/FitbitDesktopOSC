e7bef5b872f56863be0a933816fd192a7f7c6e52
Butterscotch! <bscotchvanilla@gmail.com>
2022-09-02 05:17:27 -0400

    Add basic memory dumper

---
+++ FitbitDesktopOSC/Memory.cs
@@ -0,0 +1,168 @@
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace FitbitDesktopOSC
+{
+    public class MemoryScanner
+    {
+        const int ProcessQueryInformation = 1024;
+        const int MemCommit = 4096;
+        const int PageReadWrite = 4;
+        const int ProcessWmRead = 16;
+
+        public readonly IntPtr ProcessMinAddress;
+        public readonly long ProcessMinAddressL;
+
+        public readonly IntPtr ProcessMaxAddress;
+        public readonly long ProcessMaxAddressL;
+
+        public MemoryScanner()
+        {
+            GetSystemInfo(out SystemInfo sysInfo);
+
+            ProcessMinAddress = sysInfo.MinimumApplicationAddress;
+            ProcessMinAddressL = (long)sysInfo.MinimumApplicationAddress;
+
+            ProcessMaxAddress = sysInfo.MaximumApplicationAddress;
+            ProcessMaxAddressL = (long)sysInfo.MaximumApplicationAddress;
+        }
+
+        public void Something()
+        {
+            var process = Process.GetProcessesByName("Fitbit")[0];
+
+            // Open the process with desired access level
+            var processHandle = OpenProcess(ProcessQueryInformation | ProcessWmRead, false, (uint)process.Id);
+            if (processHandle == IntPtr.Zero)
+                throw new InvalidOperationException("The process could not be loaded.");
+
+            try
+            {
+                using var sw = new StreamWriter("dump.txt");
+
+                var currentAddress = ProcessMinAddress;
+                var currentAddressL = ProcessMinAddressL;
+
+                MemoryBasicInformation memBasicInfo = new MemoryBasicInformation();
+                while (currentAddressL < ProcessMaxAddressL)
+                {
+                    if (VirtualQueryEx(processHandle, currentAddress, out memBasicInfo, Marshal.SizeOf(memBasicInfo)) <= 0)
+                    {
+                        throw new Exception("Failed to find process memory region at " + currentAddress.ToString() + ". Error code: " + Marshal.GetLastWin32Error().ToString());
+                    }
+                    var regionSize = memBasicInfo.RegionSize.ToUInt64();
+
+                    // If this memory chunk is accessible
+                    if (memBasicInfo.Protect == PageReadWrite && memBasicInfo.State == MemCommit && regionSize > 0)
+                    {
+                        var buffer = new byte[regionSize];
+                        if (!ReadProcessMemory(processHandle, memBasicInfo.BaseAddress, buffer, (uint)regionSize, out var bytesRead))
+                        {
+                            throw new Exception("Failed to read process memory at " + memBasicInfo.BaseAddress.ToString() + ". Error code: " + Marshal.GetLastWin32Error().ToString());
+                        }
+
+                        for (var i = 0; i < bytesRead; i++)
+                            sw.WriteLine("0x{0} : {1}", (memBasicInfo.BaseAddress + i).ToString("X"), (char)buffer[i]);
+                    }
+
+                    // Move to the next memory chunk
+                    currentAddressL += (long)regionSize;
+                    currentAddress = new IntPtr(currentAddressL);
+
+                    var currentValue = currentAddressL - ProcessMinAddressL;
+                    var totalValue = ProcessMaxAddressL - ProcessMinAddressL;
+                    Console.WriteLine($"{currentValue}/{totalValue} ({currentValue / (double)totalValue:0.00%})");
+                }
+            }
+            finally
+            {
+                CloseHandle(processHandle);
+            }
+        }
+
+        /// <summary>
+        /// Opens an existing local process object.
+        /// </summary>
+        /// <param name="dwDesiredAccess">The access to the process object. This access right is checked against the security descriptor for the process. This parameter can be one or more of the process access rights.</param>
+        /// <param name="bInheritHandle">If this value is TRUE, processes created by this process will inherit the handle. Otherwise, the processes do not inherit this handle.</param>
+        /// <param name="dwProcessId">The identifier of the local process to be opened.</param>
+        /// <returns>If the function succeeds, the return value is an open handle to the specified process. If the function fails, the return value is NULL.</returns>
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess([In] uint dwDesiredAccess, [In] bool bInheritHandle, [In] uint dwProcessId);
+
+        /// <summary>
+        /// Closes an open object handle.
+        /// </summary>
+        /// <param name="hObject">A valid handle to an open object.</param>
+        /// <returns>If the function succeeds, the return value is nonzero. If the function fails, the return value is 0 (zero).</returns>
+        [DllImport("kernel32.dll")]
+        public static extern int CloseHandle([In] IntPtr hObject);
+
+        /// <summary>
+        /// Reads memory.
+        /// </summary>
+        /// <param name="hProcess">The handle to the process.</param>
+        /// <param name="lpBaseAddress">The starting address to read.</param>
+        /// <param name="buffer">The byte array to hold the results.</param>
+        /// <param name="size">The number of bytes to read.</param>
+        /// <param name="lpNumberOfBytesRead">The number of bytes read.</param>
+        /// <returns>If the function succeeds, the return value is nonzero. If the function fails, the return value is 0 (zero).</returns>
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool ReadProcessMemory([In] IntPtr hProcess, [In] IntPtr lpBaseAddress, [Out] byte[] lpBuffer, [In] uint dwSize, [Out] out uint lpNumberOfBytesRead);
+
+        /// <summary>
+        /// Retrieves information about the current system.
+        /// </summary>
+        /// <param name="lpSystemInfo">A pointer to a SystemInfo structure that receives the information.</param>
+        [DllImport("kernel32.dll")]
+        static extern void GetSystemInfo([Out] out SystemInfo lpSystemInfo);
+
+        /// <summary>
+        /// Retrieves information about a range of pages within the virtual address space of a specified process.
+        /// </summary>
+        /// <param name="hProcess">A handle to the process whose memory information is queried.</param>
+        /// <param name="lpAddress">A pointer to the base address of the region of pages to be queried.</param>
+        /// <param name="lpBuffer">A pointer to a MemoryBasicInformation structure in which information about the specified page range is returned.</param>
+        /// <param name="dwLength">The size of the buffer pointed to by the lpBuffer parameter, in bytes.</param>
+        /// <returns>The return value is the actual number of bytes returned in the information buffer. If the function fails, the return value is zero.</returns>
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern int VirtualQueryEx([In] IntPtr hProcess, [In, Optional] IntPtr lpAddress, [Out] out MemoryBasicInformation lpBuffer, [In] int dwLength);
+
+        public enum ProcessorArchitecture : ushort
+        {
+            X86 = 0,
+            X64 = 9,
+            Arm = 5,
+            Itanium = 6,
+            Arm64 = 12,
+            Unknown = 0xFFFF
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SystemInfo
+        {
+            public ProcessorArchitecture ProcessorArchitecture;
+            public uint PageSize;
+            public IntPtr MinimumApplicationAddress;
+            public IntPtr MaximumApplicationAddress;
+            public UIntPtr ActiveProcessorMask;
+            public uint NumberOfProcessors;
+            public uint ProcessorType;
+            public uint AllocationGranularity;
+            public ushort ProcessorLevel;
+            public ushort ProcessorRevision;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MemoryBasicInformation
+        {
+            public IntPtr BaseAddress;
+            public IntPtr AllocationBase;
+            public uint AllocationProtect;
+            public UIntPtr RegionSize;
+            public uint State;
+            public uint Protect;
+            public uint Type;
+        }
+    }
+}
--- FitbitDesktopOSC/Program.cs
+++ FitbitDesktopOSC/Program.cs
@@ -1,2 +1,4 @@
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using FitbitDesktopOSC;
+
+var scanner = new MemoryScanner();
+scanner.Something();
